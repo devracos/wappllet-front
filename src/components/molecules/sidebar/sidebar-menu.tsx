1e84ba38cc0d727130502010bd1fe92c44c758b8
Daniel Felipe Bolivar <danideckin@gmail.com>
2024-03-18 19:47:37 -0500

    wip: UI update

--- src/components/molecules/sidebar/sidebar-menu.tsx
+++ src/components/molecules/sidebar/sidebar-menu.tsx
@@ -1,7 +1,7 @@
-import Button from '@/components/atoms/buttons/button'
 import styles from './sidebar.module.scss'
 import Image from 'next/image'
 import {pp,bills_1,bills_2,bills_3,bills_4,} from './sidebar-images'
+import SingleButton from '@/components/atoms/single-button/single-button'
 
 const SidebarMenu = ({toggleMenu}:{toggleMenu:boolean}) => {
 
@@ -10,21 +10,21 @@ const SidebarMenu = ({toggleMenu}:{toggleMenu:boolean}) => {
 			<Image className={styles.bill1} src={bills_1} alt="Bills 1" width={200}/>
 			<Image className={styles.bill2} src={bills_2} alt="Bills 2" width={200}/>
 			<Image className={styles.bill3} src={bills_3} alt="Bills 3" width={200}/>
-			<header className={styles.header}>
+			<div className={styles.header}>
 				<h1 className={styles.h1}>Wapplet</h1>
-			</header>
-			<main className={styles.main}>
+			</div>
+			<div className={styles.main}>
 				<Image className={styles.pp} src={pp} alt="Profile Image" width={180}/>
 				<h2 className={styles.h2}>Hola, bienvenido</h2>
 				<p className={styles.p}>Accede o regístrate aquí para asegurar la sincronización de tu información en múltiples dispositivos y mantener tus datos siempre disponibles.</p>
-				<Button text="Iniciar Sesión" color="normal"/>
+				<SingleButton text="Iniciar Sesión" type="normal" onClick={() => {return;}}/>
 				<div className={styles.social}>
 					<a className={styles.a}href="https://www.facebook.com/">Facebook</a>
 					<a className={styles.a}href="https://www.facebook.com/">TikTok</a>
 					<a className={styles.a}href="https://www.facebook.com/">Instagram</a>
 					<p className={styles.version}>@devracos v1.1.0</p>
 				</div>  
-			</main>
+			</div>
 		</div>
 	)
 }
