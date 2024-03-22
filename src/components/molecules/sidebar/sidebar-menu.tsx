d465099637c1ae3a6e093113dd33c24ce5649232
Daniel Felipe Bolivar <danideckin@gmail.com>
2024-03-22 18:03:15 -0500

    feat: sidebar design

--- src/components/molecules/sidebar/sidebar-menu.tsx
+++ src/components/molecules/sidebar/sidebar-menu.tsx
@@ -10,20 +10,24 @@ const SidebarMenu = ({toggleMenu}:{toggleMenu:boolean}) => {
 			<Image className={styles.bill1} src={bills_1} alt="Bills 1" width={200}/>
 			<Image className={styles.bill2} src={bills_2} alt="Bills 2" width={200}/>
 			<Image className={styles.bill3} src={bills_3} alt="Bills 3" width={200}/>
+			<Image className={styles.bill4} src={bills_4} alt="Bills 4" width={200}/>
 			<div className={styles.header}>
-				<h1 className={styles.h1}>Wapplet</h1>
+				<h2 className={styles.title}>Wapplet</h2>
 			</div>
 			<div className={styles.main}>
 				<Image className={styles.pp} src={pp} alt="Profile Image" width={180}/>
 				<h2 className={styles.h2}>Hola, bienvenido</h2>
 				<p className={styles.p}>Accede o regístrate aquí para asegurar la sincronización de tu información en múltiples dispositivos y mantener tus datos siempre disponibles.</p>
-				<SingleButton text="Iniciar Sesión" type="normal" onClick={() => {return;}}/>
+				<Image className={styles.bill4} src={bills_4} alt="Bills 4" width={200}/>
+				<SingleButton text="Iniciar Sesión" type="normal" onClick={() => {return;}}/>				  
+			</div>
+			<div className={styles.footer}>
 				<div className={styles.social}>
 					<a className={styles.a}href="https://www.facebook.com/">Facebook</a>
 					<a className={styles.a}href="https://www.facebook.com/">TikTok</a>
 					<a className={styles.a}href="https://www.facebook.com/">Instagram</a>
 					<p className={styles.version}>@devracos v1.1.0</p>
-				</div>  
+				</div>
 			</div>
 		</div>
 	)
